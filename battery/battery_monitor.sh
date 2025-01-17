60c0d19e2664069a549a5a82de22005866aebfdd
larrins <1eam@protonmail.com>
2025-01-17 13:20:56 +0530

    initial commit

---
+++ battery/battery_monitor.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+battery_capacity=$(cat /sys/class/power_supply/BAT0/capacity)
+
+if [ "$battery_capacity" -lt 10 ]; then
+    if [ ! -f /tmp/battery_10_notified ]; then
+        notify-send -u critical "Battery less than 10 percent"
+        touch /tmp/battery_10_notified
+    fi
+elif [ "$battery_capacity" -lt 20 ]; then
+    if [ ! -f /tmp/battery_20_notified ]; then
+        notify-send "Battery less than 20 percent"
+        touch /tmp/battery_20_notified
+    fi
+else
+    rm -f /tmp/battery_10_notified /tmp/battery_20_notified
+fi
